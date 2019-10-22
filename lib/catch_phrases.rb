34e4db08461f9e726b9bf5d8cc5f253115a87778
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-22 19:20:13 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,22 @@
+the_roll = "Do A Barrel Roll"
+
 def mario
+  phrase = "It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts status
+end
+
+def link 
+  status = "It's Dangerous To Go Alone! Take This."
+  puts status
+end
+
+def fox (the_roll)
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
